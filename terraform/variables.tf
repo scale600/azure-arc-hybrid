1c5b109225ac0ce56ea0db818deb0a4516e24d51
Richard Lee <scale600@gmail.com>
2026-09-16 12:28:22 -0700

    M9: switch VM size to Standard_B1s (B1ls capacity-restricted in koreacentral)

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -33,7 +33,7 @@ variable "vm_name" {
 variable "vm_size" {
   description = "Azure VM size"
   type        = string
-  default     = "Standard_B1ls"
+  default     = "Standard_B1s"
 }
 
 variable "admin_ssh_public_key" {
